fc47eadac54b687736ecc3524329768be9b643cc
Luis Medina <luisinmedina@gmail.com>
2022-05-07 13:43:56 -0400

    Add MUI-StoryBook libraries and set new component

---
+++ src/stories/components/CytyInfo/CityInfo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Typography from '@mui/core/Typography'
+const CityInfo = ({city, country}) => {
+  return (
+    <>
+        <Typography>{city}</Typography>
+        <Typography>{country}</Typography>
+    </>
+  )
+}
+
+CityInfo.propTypes = {
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired
+}
+
+export default CityInfo
